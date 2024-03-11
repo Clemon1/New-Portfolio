d7fad636005b2f04e04b10925afab3b88cde2ce2
CLEMON EZEH <clemonezeh@gmail.com>
2024-03-11 10:35:35 +0100

    added my image

--- src/component/AboutPage.jsx
+++ src/component/AboutPage.jsx
@@ -1,5 +1,6 @@
 import { Box, Divider, Flex, Image, Text } from "@chakra-ui/react";
 import blob from "../assets/blobanimation.svg";
+import blob2 from "../assets/blobClem.svg";
 
 const AboutPage = () => {
   return (
@@ -18,12 +19,28 @@ const AboutPage = () => {
         width={"full"}
         height={"fit-content"}
         direction={["column", "column", "column", "row", "row"]}>
-        <Flex width={"full"} direction={"column"} alignItems={"flex-start"}>
-          <Image width={"full"} height={"75%"} src={blob} />
+        <Flex
+          width={"full"}
+          direction={"column"}
+          position={"relative"}
+          alignItems={"flex-start"}>
+          <Image
+            width={"full"}
+            height={["60%", "65%", "80%", "80%", "80%"]}
+            src={blob}
+            zIndex={100}
+          />
+          <Image
+            width={"full"}
+            height={["75%", "75%", "75%", "75%", "75%"]}
+            src={blob2}
+            zIndex={2000}
+            position={"absolute"}
+          />
         </Flex>
         <Flex
           width={"full"}
-          height={"80%"}
+          height={"60vh"}
           justifyContent={"left"}
           alignItems={"center"}>
           <Text
